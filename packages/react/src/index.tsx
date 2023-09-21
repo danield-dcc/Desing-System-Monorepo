f2c28f5b7effa66b059e1a38aad2d16c1448b5f9
Daniel <danieldcc.colares@gmail.com>
2023-09-21 20:01:55 -0300

    initial commit

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { colors } from '@ignite-ui/tokens'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$gray500',
+  borderRadius: '$md',
+  padding: '$4',
+})
---
+++ redme.md
@@ -0,0 +1,11 @@
+# Componentes
+
+- [ ] Text - tamanho da fonte, cor, fonte
+- [ ] Heading - Cabeçahos
+- [ ] Box / Card - Componente facilmente reutilizável
+- [ ] Button -
+- [ ] TextInput
+- [ ] CheckBox
+- [ ] TextArea
+- [ ] Avatar
+- [ ] MultiStep
