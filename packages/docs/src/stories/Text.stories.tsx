409fcf4ca829fbed701a24f0bd3af47df41ad0ad
Daniel <danieldcc.colares@gmail.com>
2023-09-22 16:46:38 -0300

    text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum beatae et sunt tempora, hic, id libero consequuntur veniam voluptas repellendus eius delectus at laboriosam quos quae nihil, voluptate quo quam!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
