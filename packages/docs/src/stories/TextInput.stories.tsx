7e09472c58294c08535ce941eaf1741668cd6848
Daniel <danieldcc.colares@gmail.com>
2023-09-23 12:24:27 -0300

    🎨 Adds TextInput component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">E-mail address</Text>
+          {Story()} {/* Story é a renderização do input */}
+        </Box>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'call.com/',
+  },
+}
