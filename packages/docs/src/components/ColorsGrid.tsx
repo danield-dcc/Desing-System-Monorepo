f4e54859747ce6b8c13d3f2eca3bb7f326816689
Daniel <danieldcc.colares@gmail.com>
2023-09-30 09:04:26 -0300

    fix: change package names

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,5 @@
-import { colors } from '@ignite-ui/tokens'
+import React from 'react'
+import { colors } from '@danieldcc-ignite-ui/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
